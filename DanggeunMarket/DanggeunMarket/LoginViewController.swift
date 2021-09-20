d45cd27031d1659e9a23a86e10f60e504d8f900d
Yujin Yang <s2019112541@mme.dongguk.edu>
2021-09-21 02:13:36 +0900

    LoginView

--- DanggeunMarket/DanggeunMarket/LoginViewController.swift
+++ DanggeunMarket/DanggeunMarket/LoginViewController.swift
@@ -9,17 +9,64 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var idTextField: UITextField!
     @IBOutlet weak var pwTextField: UITextField!
+    @IBOutlet var textFields: [UITextField]!
+    
+    weak var activeField: UITextField?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         loginButton.backgroundColor = UIColor(red: 255/255, green: 138/255, blue: 61/255, alpha: 1)
         loginButton.layer.cornerRadius = 10
+        
+        for textField in textFields{
+            textField.delegate = self
+        }
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardAppear), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDisappear), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
+        tapGestureRecognizer.numberOfTapsRequired = 1
+        tapGestureRecognizer.isEnabled = true
+        scrollView.addGestureRecognizer(tapGestureRecognizer)
 
         
+    }
+    
+    @objc func viewTapped() {
+         if let activatedField = activeField {
+             activatedField.resignFirstResponder()
+         }
+     }
+
+     @objc func keyboardDisappear(notification: Notification) {
+        // UIResponder.keyboardWillHideNotification 을 NotificationCenter에서 감지했을 때 수행할 메소드 내용 (키보드가 사라질 예정이다.)\
+        let contentInsets = UIEdgeInsets.zero
+        scrollView.contentInset = contentInsets
+        scrollView.verticalScrollIndicatorInsets = contentInsets
+     }
+    
+    @objc func keyboardAppear(notification: Notification) {
+        guard let activeField = activeField,
+              let userInfo = notification.userInfo,
+              let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else {return}
+    
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardFrame.height, right: 0.0)
+        scrollView.contentInset = contentInsets
+        scrollView.verticalScrollIndicatorInsets = contentInsets
+        let activeRect = activeField.convert(activeField.bounds, to: scrollView)
+        scrollView.scrollRectToVisible(activeRect, animated: true)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let activatedField = activeField {
+                activatedField.resignFirstResponder()
+        }
     }
     
     @IBAction func ClickToLogin(_ sender: Any) {
@@ -30,7 +77,20 @@ class LoginViewController: UIViewController {
         self.present(nav, animated: true, completion: nil)
     }
     
+}
 
-
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        activeField = nil
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        activeField = textField
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
