aaa0a0f44cc3b54fcde5a504a69069abdec46eef
Yujin Yang <s2019112541@mme.dongguk.edu>
2021-09-23 20:51:14 +0900

    Set AddVC

--- DanggeunMarket/DanggeunMarket/Home/AddViewController.swift
+++ DanggeunMarket/DanggeunMarket/Home/AddViewController.swift
@@ -7,15 +7,38 @@
 
 import UIKit
 
-class AddViewController: UIViewController {
+class AddViewController: UIViewController, UITextViewDelegate{
 
+    @IBOutlet weak var albumBtn: UIButton!
+    @IBOutlet weak var productImg: UIImageView!
+    @IBOutlet weak var infoTextField: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        albumBtn.layer.borderColor = UIColor.gray.cgColor
+        albumBtn.layer.borderWidth = 0.3
+        albumBtn.layer.cornerRadius = 5
+        
+
+        infoTextField.delegate = self
+        infoTextField.isScrollEnabled = false
+        textViewDidChange(infoTextField)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         self.tabBarController?.tabBar.isHidden = true
     }
     
+    func textViewDidChange(_ textView: UITextView) {
+
+        let size = CGSize(width: view.frame.width, height: .infinity)
+        let estimatedSize = textView.sizeThatFits(size)
+        textView.constraints.forEach { (constraint) in
+            if constraint.firstAttribute == .height {
+                constraint.constant = estimatedSize.height
+            }
+        }
+    }
+    
 }
+
