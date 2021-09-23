e02c1939100b118fe82faf28213167b96652f109
Yujin Yang <s2019112541@mme.dongguk.edu>
2021-09-23 12:31:29 +0900

    Added product info

--- DanggeunMarket/DanggeunMarket/Home/DetailViewController.swift
+++ DanggeunMarket/DanggeunMarket/Home/DetailViewController.swift
@@ -27,13 +27,14 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         
         setView()
-//        self.navigationController!.navigationBar.setBackgroundImage(UIImage(), for: .default)
-//         self.navigationController!.navigationBar.shadowImage = UIImage()
-//         self.navigationController!.navigationBar.isTranslucent = true
         
-        print(index)
         profileImg.layer.cornerRadius = profileImg.frame.height/2
         profileImg.clipsToBounds = true
+        
+        //        self.navigationController!.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        //         self.navigationController!.navigationBar.shadowImage = UIImage()
+        //         self.navigationController!.navigationBar.isTranslucent = true
+                
 
     }
     
@@ -46,7 +47,29 @@ class DetailViewController: UIViewController {
         topView.layer.borderWidth = 0.3
         bottomView.layer.borderColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5).cgColor
         bottomView.layer.borderWidth = 0.3
-//        chatButton.backgroundColor = UIColor(red: 255/255, green: 138/255, blue: 61/255, alpha: 1)
+        
+        // infoLabel Custom
+        infoLabel.numberOfLines = 0
+
+        let font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 6
+        
+        // 한글 줄바꿈 적용
+        if #available(iOS 14.0, *) {
+            paragraphStyle.lineBreakStrategy = .hangulWordPriority
+        }
+                
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: font,
+            .foregroundColor: UIColor.black,
+            .paragraphStyle: paragraphStyle
+        ]
+        infoLabel.attributedText = NSAttributedString(string: p.productArray[index].info, attributes: attributes)
+        
+        // Custom UIButton
+        chatButton.backgroundColor = UIColor(red: 255/255, green: 138/255, blue: 61/255, alpha: 1)
+        chatButton.layer.cornerRadius = 5
         
         productImg.image = p.productArray[index].productImage
         idLabel.text = p.productArray[index].id
--- DanggeunMarket/DanggeunMarket/Model/ProductSingleton.swift
+++ DanggeunMarket/DanggeunMarket/Model/ProductSingleton.swift
@@ -22,11 +22,11 @@ struct productInfo{
 class product {
     static let shared = product()
     var productArray:[productInfo] = [
-        productInfo(id: "화이팅", productImage: UIImage(named: "kal.jpg")! , productTitle: "칼하트 후드집업" , price: 40000, location: "당산동", info: "사이즈는 XL입니다. 상태는 최상입니다.", date: "21년 9월 20일 2시 20분 41초"),
+        productInfo(id: "화이팅", productImage: UIImage(named: "kal.jpg")! , productTitle: "칼하트 후드집업" , price: 40000, location: "당산동", info: "사이즈는 XL입니다. 하자 없고 완전 새상품이고 상태는 최상‼️입니다. 택배비 별도", date: "21년 9월 20일 2시 20분 41초"),
         productInfo(id: "야니", productImage: UIImage(named: "tumb.jpg")! , productTitle: "스타벅스 텀블러 (미사용)" , price: 50000, location: "여의도동", info: "스타벅스 미니 콜라보 투고 텀블러입니다.", date: "21년 9월 20일 2시 20분 41초"),
-        productInfo(id: "민", productImage: UIImage(named: "xs.jpeg")! , productTitle: "아이폰 XS 64GB 스페이스 그레이 팔아요" , price: 320000, location: "양평동", info: "연락주세요.", date: "21년 9월 20일 2시 25분 55초"),
-        productInfo(id: "도비", productImage: UIImage(named: "ably.jpg")! , productTitle: "에이블리 얇은 크롭 가디건" , price: 7000, location: "구로구", info: "여름 옷 정리하려고 팔아요! 원가 14800원입니다!", date: "21년 9월 20일 2시 20분 41초"),
-        productInfo(id: "영화보러갈꺼야", productImage: UIImage(named: "key.jpg")! , productTitle: "로지텍 블루투스 키보드 k480" , price: 20000, location: "동작구 신대방제1동", info: "이틀사용했고 이상없습니다", date: "21년 9월 20일 2시 20분 41초")
+        productInfo(id: "민", productImage: UIImage(named: "xs.jpeg")! , productTitle: "아이폰 XS 64GB 스페이스 그레이 팔아요" , price: 320000, location: "양평동", info: "배터리 효율 84%입니다. 항상 케이스를 끼고 다녀서 깨끗하지만 사진에서 보신 거와 같이 생활기스 몇 개 있습니다. 살 때 받은 박스에 담아서 드립니다.", date: "21년 9월 20일 2시 25분 55초"),
+        productInfo(id: "도비", productImage: UIImage(named: "ably.jpg")! , productTitle: "에이블리 얇은 크롭 가디건" , price: 7000, location: "구로구", info: "한 번도 입은적 없어요! 여름 옷 정리하려고 팔아요! ✅원가 14800원입니다!✅", date: "21년 9월 20일 2시 20분 41초"),
+        productInfo(id: "영화보러갈꺼야", productImage: UIImage(named: "key.jpg")! , productTitle: "로지텍 블루투스 키보드 k480" , price: 20000, location: "동작구 신대방제1동", info: "이틀사용했고 이상없습니다. 상품을 직접 보시고 거래하는게 좋을 것 같아서 직거래를 선호합니다. 혹시 보고 마음에 안드시면 구매하지 않으셔도 됩니다~", date: "21년 9월 20일 2시 20분 41초")
     ]
     
 }
